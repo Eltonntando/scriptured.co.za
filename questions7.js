3207961e6b1831c77e73b4e4b98f992d33b65444
Ntando Elton <77546309+Eltonntando@users.noreply.github.com>
2023-05-05 17:51:31 +0200

    Add files via upload

---
+++ questions7.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Up-to how many books does the bible have?",
+    answer: "66 books",
+    options: [
+      "59 books",
+      "66 books",
+      "76 books",
+      "60 books"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The bible is divided into how many Testaments?",
+    answer: "2 Testaments",
+    options: [
+      "10 Testaments",
+      "1 Testament",
+      "3 Testaments",
+      "2 Testaments"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which one is the first book in the bible?",
+    answer: "Genesis",
+    options: [
+      "Mathew",
+      "Exodus",
+      "Revelations",
+      "Genesis"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which one is the last book in the bible?",
+    answer: "Revelations",
+    options: [
+      "John",
+      "Revelations",
+      "Genesis",
+      "Chronicles"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who was the first person that God created?",
+    answer: "Adam",
+    options: [
+      "Joseph",
+      "Noah",
+      "Adam",
+      "Abel"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
