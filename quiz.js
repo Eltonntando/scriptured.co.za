cc41f7c16e8a73d9f26cf36fffff26c17992f613
Ntando Elton <77546309+Eltonntando@users.noreply.github.com>
2023-07-11 19:33:53 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -23,10 +23,10 @@ continue_btn.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
-    startTimer(20); //calling startTimer function
+    startTimer(40); //calling startTimer function
     startTimerLine(0); //calling startTimerLine function
 }
-let timeValue =  20;
+let timeValue =  40;
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
@@ -39,7 +39,7 @@ const quit_quiz = result_box.querySelector(".buttons .quit");
 restart_quiz.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); //show quiz box
     result_box.classList.remove("activeResult"); //hide result box
-    timeValue = 20; 
+    timeValue = 40; 
     que_count = 0;
     que_numb = 1;
     userScore = 0;
@@ -178,11 +178,11 @@ function startTimer(time){
     }
 }
 function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
+    counterLine = setInterval(timer, 40);
     function timer(){
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
+        if(time > 800){ //if time value is greater than 549
             clearInterval(counterLine); //clear counterLine
         }
     }
